052671be3bfe82413577e290623794ed366ad08b
sargam-poudel <sargampoudel100@gmail.com>
2021-07-30 22:58:05 +0545

    [added] status feature

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,15 +1,26 @@
 import discord
 from discord import message
-from discord.ext import commands
-
+from discord.ext import commands, tasks
+from itertools import cycle
 
 class Utilis(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.status = cycle(
+            ["minecraft", 
+            "among us"]
+            )
 
     @commands.Cog.listener()
     async def on_ready(self):
+        self.change_status.start()
         print("Logged in as bot")
+
+
+    @tasks.loop(seconds=5)
+    async def change_status(self):
+        await self.client.change_presence(activity=discord.Game(next(self.status)))
+
 
     @commands.command()
     async def ping(self, ctx):

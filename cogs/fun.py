53e2b92d2a0177003ef7512ea932d7c0502b5918
sargam-poudel <sargampoudel100@gmail.com>
2021-08-27 17:11:27 +0545

    [edited] added default embed color & made changes to the data representation

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,25 +3,36 @@ from discord.ext import commands
 
 from random import randrange
 from datetime import date
-
 from typing import Optional
+
+from config import DEFAULT_EMBED_COLOR
 
 
 class Fun(commands.Cog):
+    """
+    Some Fun commands
+    """
+
     def __init__(self, client) -> None:
         self.client = client
+        self.embed_color = DEFAULT_EMBED_COLOR
+
+    def pp(self) -> str:
+        pp_str = "8"
+        for _ in range(randrange(10)):
+            pp_str += "="
+        pp_str += "D"
+        return pp_str
 
     @commands.command()
     async def pp(self, ctx, user: Optional[discord.Member] = None) -> None:
         if user is None:
             user = ctx.author
-        pp_str = "8"
-        for _ in range(randrange(10)):
-            pp_str += "="
-        pp_str += "D"
+
         pp_embed = discord.Embed(
             title="PP size machine",
-            description=f"{user.name}'s pp size:\n {pp_str}"
+            description=f"{user.name}'s pp size:\n {self.pp()}",
+            colour=self.embed_color
         )
         await ctx.send(embed=pp_embed)
 
@@ -33,29 +44,30 @@ class Fun(commands.Cog):
     async def device(self, ctx: commands.context, user: Optional[discord.Member] = None) -> None:
         if user is None:
             user = ctx.author
-        comp_status = user.desktop_status   
+        comp_status = user.desktop_status
         mobile_status = user.mobile_status
         web_status = user.web_status
-        
+
         embed = discord.Embed(
-            title = f"{user.display_name}'s device status",
-            colour = 0x01a6ff,
+            title=f"{user.display_name}'s device status",
+            colour=self.embed_color
         )
         embed.add_field(
-            name = "PC client", 
-            value = f"💻: {comp_status}",
-            inline =True)
+            name="PC client",
+            value=f"💻: {comp_status}",
+            inline=True)
         embed.add_field(
-            name = "Web client", 
-            value = f"🌏: {web_status}",
-            inline = False)
+            name="Web client",
+            value=f"🌏: {web_status}",
+            inline=False)
         embed.add_field(
-            name = "Mobile client", 
-            value = f"📱: {mobile_status}",
-            inline = False)
+            name="Mobile client",
+            value=f"📱: {mobile_status}",
+            inline=False)
 
         embed.set_thumbnail(url=user.avatar_url)
-        await ctx.send(embed = embed)
-        
+        await ctx.send(embed=embed)
+
+
 def setup(client):
     client.add_cog(Fun(client))

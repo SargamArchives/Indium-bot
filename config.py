5f59500fef5235fe244bab20745bf99526c92a4f
sargam-poudel <sargampoudel100@gmail.com>
2021-08-27 16:38:52 +0545

    [added] Default prefix, embed & activity in config

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_KEY = os.environ.get("API_KEY")
 ID1 = int(os.environ.get("ID1"))
 ID2 = int(os.environ.get("ID2"))
+
+# General configuration for bot
+DEFAULT_PREFIX = ">"
+ACTIVITY_STATUS = [
+    f"playing prefix {DEFAULT_PREFIX}",
+    "watching your activity"
+]
+DEFAULT_EMBED_COLOR = 0x01a6ff

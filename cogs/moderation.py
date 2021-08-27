6e7fadd6a706859e577eac23b0a330a48eb4e582
sargam-poudel <sargampoudel100@gmail.com>
2021-08-27 17:17:21 +0545

    [edited] default embed color & fixed delete cooldown

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,19 +1,28 @@
 import discord
+from discord import colour
 from discord.ext import commands
+
 from asyncio import sleep
 
+from config import DEFAULT_EMBED_COLOR
 
 class Moderation(commands.Cog):
     def __init__(self, client):
-        self.client = client
+        self.client = client,
+        self.embed_color = DEFAULT_EMBED_COLOR
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx: commands.Context, user: discord.Member, *, reason="No reason provided"):
+    async def kick(self,
+                   ctx: commands.Context,
+                   user: discord.Member,
+                   *, reason="No reason provided"):
+
         await user.kick(reason=reason)
         kick = discord.Embed(
             title=f":boot: Kicked {user.display_name}!",
-            description=f"Reason: {reason}\nBy: {ctx.author.mention}"
+            description=f"Reason: {reason}\nBy: {ctx.author.mention}",
+            colour=self.embed_color
         )
         await ctx.message.delete()
         await ctx.channel.send(embed=kick)
@@ -21,11 +30,16 @@ class Moderation(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx: commands.Context, user: discord.Member, *, reason: str ="No reason Provided"):
+    async def ban(self, 
+                  ctx: commands.Context, 
+                  user: discord.Member, 
+                  *, reason: str = "No reason Provided"):
+
         await user.ban(reason=reason)
         ban = discord.Embed(
             title=f":hammer: Banned {user.name}!",
-            description=f"Reson: {reason}\nBy: {ctx.author.mention}"
+            description=f"Reson: {reason}\nBy: {ctx.author.mention}",
+            colour=self.embed_color
         )
         message = await ctx.send(embed=ban)
         await user.send(embed=ban)
@@ -34,16 +48,19 @@ class Moderation(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def purge(self, ctx: commands.Context,  amount: int):
+    async def purge(self, 
+                    ctx: commands.Context,  
+                    amount: int):
         if amount < 1:
             return
         await ctx.channel.purge(limit=amount + 1)
         purge = discord.Embed(
             title=f"Message deleted",
-            description=f"{amount} message deleted by {ctx.author.mention}"
+            description=f"{amount} message deleted by {ctx.author.mention}",
+            colour=self.embed_color
         )
         message = await ctx.channel.send(embed=purge)
-        await sleep(5)
+        await sleep(10)
         await message.delete()
 
 

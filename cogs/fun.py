136697e71fa5c1d4928438f5a3af24ad81ec47ef
sargam-poudel <sargampoudel100@gmail.com>
2021-08-30 23:37:23 +0545

    [added] server info command

--- cogs/fun.py
+++ cogs/fun.py
@@ -5,8 +5,6 @@ from random import randrange
 from datetime import date
 from typing import Optional, Union
 
-from discord.member import Member
-
 from config import DEFAULT_EMBED_COLOR
 
 
@@ -59,5 +57,33 @@ class Fun(commands.Cog):
         embed.set_thumbnail(url=user.avatar_url)
         await ctx.send(embed=embed)
 
+    @commands.command(name="serverinfo")
+    async def server_info(self, ctx: commands.Context):
+        guild: discord.Guild = ctx.guild
+        name = guild.name
+        owner = guild.owner
+        server_icon = guild.icon_url
+        catagories = len(guild.categories)
+        text_channel = len(guild.text_channels)
+        voice_channel = len(guild.voice_channels)
+        members = guild.member_count
+        id = guild.id
+        created_at = guild.created_at.date()
+        roles = len(guild.roles)
+        embed = discord.Embed(
+            color=self.embed_color
+        )
+        embed.set_author(name=f"{name}", icon_url=f"{server_icon}")
+        embed.add_field(name = "Server Owner", value=f"{owner}")
+        embed.add_field(name = "Channel Categories", value=f"{catagories}")
+        embed.add_field(name = "Text Channels", value=f"{text_channel}")
+        embed.add_field(name = "Voice Channels", value=f"{voice_channel}")
+        embed.add_field(name = "Member count", value=f"{members}")
+        embed.add_field(name = "Role count", value=f"{roles}")
+        embed.set_thumbnail(url = server_icon)
+        embed.set_footer(text = f"ID: {id} | Server Created at • {created_at}")
+        await ctx.send(embed=embed)
+
+        
 def setup(client):
     client.add_cog(Fun(client))

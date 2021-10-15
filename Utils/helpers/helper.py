1aa84f12e0defd220b6137d3f4d78b4fb3219694
sargam-poudel <sargampoudel100@gmail.com>
2021-10-15 22:10:51 +0545

    code that checks for owner id's from the config file

---
+++ Utils/helpers/helper.py
@@ -0,0 +1,8 @@
+from discord.ext.commands.errors import NotOwner
+from Config.config import OWNER_ID
+
+
+def check_owner(ctx):
+    if ctx.author.id not in OWNER_ID:
+        raise NotOwner("You are not the owner of the bot.")
+    return True
